2dada3578215d0cec0098d93119fd056e9135517
MarcelineRll <heladoloco064@gmail.com>
2024-06-02 03:40:11 -0400

    added test to Player.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from modules.Player import Player
 
 def main() -> None:
     new_player = Player(Vector2(0, 0))
+    print(new_player.position)
 
 if __name__ == "__main__":
     main()
+
--- src/modules/Player.py
+++ src/modules/Player.py
@@ -3,3 +3,11 @@ from pygame.math import Vector2
 class Player:
     def __init__(self, position: Vector2):
         self.position = position or Vector2(0, 0)
+
+if __name__ == "__main__":
+    print("Testing Player Class...")
+    print("Creating a new player..")
+    new_player = Player(Vector2(0, 0))
+    print(f"new_player's position: {new_player.position}")
+    print("tests passed")
+
